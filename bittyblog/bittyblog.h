00a866cf7bc331beab56026e65a7ae3e3b12c5d8
cluoma <themjolnir999@gmail.com>
2019-03-17 20:36:27 +0100

    fix enum

--- bittyblog/bittyblog.h
+++ bittyblog/bittyblog.h
@@ -23,7 +23,7 @@ enum bb_page_styles {
     BLOG_FULL_POST  = 0,
     BLOG_SMALL_POST = 1,
     CONTACT         = 2,
-    MISSING         = 4
+    MISSING         = 3
 };
 
 // Information on the type of webpage
