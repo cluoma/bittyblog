bfffec89b50cefaf0ba9c32c54bec57759d49335
cluoma <themjolnir999@gmail.com>
2019-04-13 22:01:53 +0200

    Remove fonts from setup script

--- setup.sh
+++ setup.sh
@@ -28,8 +28,7 @@ make all
 echo .. Copying executables
 mv bb.cgi www/cgi-bin/bb.cgi
 mv bbadmin.cgi www/cgi-bin/bbadmin.cgi
-echo .. Copying fonts, images, and css
-cp -r fonts www/
+echo .. Copying images, and css
 cp -r css www/
 cp -r images www/
 
